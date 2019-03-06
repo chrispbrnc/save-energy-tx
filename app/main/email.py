3587ecf4beae1f490838cecbad7486ad85f1805a
Patrick Burris <nchupacabra@gmail.com>
2019-03-06 16:54:47 -0600

    edit 404 and 550 pages and change more info text

--- app/main/email.py
+++ app/main/email.py
@@ -8,3 +8,9 @@ def send_moreinfo_email(email):
         recipients=email,
         text_body=render_template('email/more_info/more_info.txt'),
         html_body=render_template('email/more_info/more_info.html'))
+
+    send_email('[Energy Savers] Information Request',
+        sender=current_app.config['ADMINS'][0],
+        recipients=app.config['ADMINS'][1],
+        text_body=render_template('email/more_info/more_info_admin.txt'),
+        html_body=render_template('email/more_info/more_info_admin.html'))
